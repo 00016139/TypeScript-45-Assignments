ceed040ebd03c94162e3ad4a92d71e7c890543ea
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:52:18 +0500

    Add files via upload
    Question No_11

---
+++ Names.js
@@ -0,0 +1,4 @@
+var Name = ["Aqib", "Fazeel", "Subhan", "Kashan"]; // declare a varible having some names
+for (var i = 0; i < Name.length; i++) {
+    console.log(Name[i]);
+}
---
+++ Names.ts
@@ -0,0 +1,4 @@
+let Name : String[] = ["Aqib", "Fazeel", "Subhan", "Kashan"]; // declare a varible having some names
+for (let i = 0; i < Name.length; i++) {
+    console.log(Name [i]);
+} 
