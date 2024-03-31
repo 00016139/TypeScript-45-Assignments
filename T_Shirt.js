c8f93956ff3ec55b0b8b99465bf0522115d92c96
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:05:52 +0500

    Add files via upload
    Question No_36

---
+++ T_Shirt.js
@@ -0,0 +1,4 @@
+function Make_Shirt(Size, Messege) {
+    console.log("Making a ".concat(Size, " size T_Shirt with the messege ").concat(Messege, " printed on it"));
+}
+Make_Shirt("Medium", "Apna Time Ayega!");
---
+++ T_Shirt.ts
@@ -0,0 +1,4 @@
+function Make_Shirt(Size : string, Messege : string) {
+    console.log(`Making a ${Size} size T_Shirt with the messege ${Messege} printed on it`);
+}
+Make_Shirt("Medium", "Apna Time Ayega!")
