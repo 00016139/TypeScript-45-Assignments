8c4185ea29f472f4a8047a9e6eb9823ebad456ff
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:53:19 +0500

    Add files via upload
    Question No_13

---
+++ Own_Array.js
@@ -0,0 +1,4 @@
+var Transports = ["Bike", "Car", "Plane"];
+Transports.forEach(function (Transport) {
+    console.log("I want to buy ".concat(Transport));
+});
---
+++ Own_Array.ts
@@ -0,0 +1,4 @@
+let Transports : string[] = ["Bike", "Car", "Plane"]; // declare a variable of transport
+Transports.forEach(Transport => {
+    console.log(`I want to buy ${Transport}`);
+}) // use foreach function to print seprately
