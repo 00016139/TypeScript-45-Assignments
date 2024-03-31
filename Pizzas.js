f865ad6bb0a47d8ea70c9260aaa723a706338a0a
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:04:58 +0500

    Add files via upload
    Question No_34

---
+++ Pizzas.js
@@ -0,0 +1,8 @@
+var Pizzas = ["Chicken Fajita", "Malai Boti Pizza", "Creamy Tikka"];
+Pizzas.forEach(function (Pizza) {
+    console.log(Pizza);
+});
+Pizzas.forEach(function (Pizza) {
+    console.log("I Like ".concat(Pizza, " Pizza"));
+});
+console.log("I Realy Love Pizza!");
---
+++ Pizzas.ts
@@ -0,0 +1,10 @@
+let Pizzas :string[] = ["Chicken Fajita", "Malai Boti Pizza", "Creamy Tikka"];
+
+Pizzas.forEach( Pizza => {
+console.log(Pizza);
+})
+
+Pizzas.forEach(Pizza => {
+    console.log(`I Like ${Pizza} Pizza`);
+})
+console.log("I Realy Love Pizza!");
