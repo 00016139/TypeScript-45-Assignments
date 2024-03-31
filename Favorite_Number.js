77b861b9dd4d5f5d1ce231ac135979097d787d52
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:51:19 +0500

    Add files via upload
    Question No_9

---
+++ Favorite_Number.js
@@ -0,0 +1,3 @@
+var Mynum = 5; // declare a variable type number
+console.log("My Favorite Number is = ".concat(Mynum));
+// print the variable with message
---
+++ Favorite_Number.ts
@@ -0,0 +1,3 @@
+let Mynum : Number = 5; // declare a variable type number
+console.log(`My Favorite Number is = ${Mynum}`);
+// print the variable with message
