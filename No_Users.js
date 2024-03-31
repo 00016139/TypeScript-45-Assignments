846e45d36ff226927e49742f9ead61d25c2c34b2
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:03:24 +0500

    Add files via upload
    Question No_31

---
+++ No_Users.js
@@ -0,0 +1,7 @@
+var Greetings = [];
+if (Greetings.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    console.log("Hello");
+}
---
+++ No_Users.ts
@@ -0,0 +1,7 @@
+let Greetings :string[] = [];
+
+if(Greetings.length === 0) {
+    console.log("We need to find some users!");
+} else {
+console.log("Hello Users");
+}
