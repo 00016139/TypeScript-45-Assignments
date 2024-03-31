0253da19efee0b680853f04c2d52e01e008597f6
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:02:24 +0500

    Add files via upload
    Question No_29

---
+++ Favorite_Fruit.js
@@ -0,0 +1,10 @@
+var Favorite_Fruit = ["Mango", "Strawberry", "Orange"];
+if (Favorite_Fruit.includes("Mango")) {
+    console.log("I love \"Mango\"");
+}
+if (Favorite_Fruit.includes("Strawberry")) {
+    console.log("I Realy love \"Strawberry\"");
+}
+if (Favorite_Fruit.includes("Orange")) {
+    console.log("I want to eat \"Oranges\"");
+}
---
+++ Favorite_Fruit.ts
@@ -0,0 +1,14 @@
+let Favorite_Fruit : string[] = ["Mango", "Strawberry", "Orange"];
+// declare a variable having some fruit names / array[]
+
+if(Favorite_Fruit.includes("Mango")) {
+    console.log(`I love "Mango"`);
+} // condition if mango include in variable then print the messege
+
+if(Favorite_Fruit.includes("Strawberry")) {
+    console.log(`I Realy love "Strawberry"`);
+} // condition if strawberrey include in variable then print the messege
+
+if(Favorite_Fruit.includes("Orange")) {
+    console.log(`I want to eat "Oranges"`);
+} // condition if orange include in variable then print the messege
