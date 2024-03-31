e530dd5ea9fa736ca09ac572619df446c8d0ace1
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:00:30 +0500

    Add files via upload
    Question No_26

---
+++ Alien_Color_2.js
@@ -0,0 +1,13 @@
+var Alient_Color = "Red";
+if (Alient_Color == "Red") {
+    console.log("You Got It...!!!, Shoot more Aliens");
+}
+else {
+    console.log("You got 100 Points");
+}
+if (Alient_Color == "Blue") {
+    console.log("You Lost...!!!, Bed Luck");
+}
+else {
+    console.log("You got 0 Point");
+}
---
+++ Alien_Color_2.ts
@@ -0,0 +1,16 @@
+let Alient_Color :string = "Red";
+
+if(Alient_Color == "Red") {
+    console.log(`You Got It...!!!, Shoot more Aliens`);
+} else {
+    console.log("You got 100 Points");
+}
+// Codition pass print 1st messege
+
+
+if(Alient_Color == "Blue") {
+    console.log("You Lost...!!!, Bed Luck");
+} else {
+console.log("You got 0 Point");
+}
+// Condition fail, print the second messege
