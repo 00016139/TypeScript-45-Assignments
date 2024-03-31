4f157648563edf72002903bfb1b9e1b4aa6a8fee
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:57:34 +0500

    Add files via upload
    Question No_21

---
+++ Something_in_TypeScript.ts
@@ -0,0 +1,8 @@
+let Variable : {Country : String; Event : string; Date : number;} = {
+Country : "Pakistan's",
+Event : "Resolution",
+Date : 23};
+// declare a variable and store different type of data 
+
+
+console.log(`${Variable.Country} ${Variable.Event} day Celebrated on ${Variable.Date}rd March`);
---
+++ Something_in_Typescript.js
@@ -0,0 +1,6 @@
+var Variable = {
+    Country: "Pakistan's",
+    Event: "Resolution",
+    Date: 23
+};
+console.log("".concat(Variable.Country, " ").concat(Variable.Event, " day Celebrated on ").concat(Variable.Date, "rd March"));
