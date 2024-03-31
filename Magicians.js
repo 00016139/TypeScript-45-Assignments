460848bc92097c4cbd6a52bdd5fc04d3f3a790c4
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:09:26 +0500

    Add files via upload
    Question No_41

---
+++ Magicians.js
@@ -0,0 +1,7 @@
+var Magicians = ["Aqib", "Fazeel", "Subhan"];
+function Show_Magicians(Magicians) {
+    Magicians.forEach(function (Magicians) {
+        console.log(Magicians);
+    });
+}
+Show_Magicians(Magicians);
---
+++ Magicians.ts
@@ -0,0 +1,8 @@
+let Magicians : string[] = ["Aqib", "Fazeel", "Subhan"];
+
+function Show_Magicians(Magicians : String[]) {
+    Magicians.forEach(Magicians => {
+        console.log(Magicians);
+    });
+}
+Show_Magicians(Magicians);
