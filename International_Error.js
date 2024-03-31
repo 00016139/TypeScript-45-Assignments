c947536ae22b1e32ce8a18ba12ca027dc2b6f3a6
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:58:00 +0500

    Add files via upload
    Question No_22

---
+++ International_Error.js
@@ -0,0 +1,4 @@
+var Variable = ["Biryani", "Chicken Karahi", "Kunafa"];
+console.log("I want to eat ".concat(Variable[4]));
+// Variable 4 print undefined, becasue varible having only 3 items zero 1 and 2,
+console.log("I want to eat ".concat(Variable[2]));
---
+++ International_Error.ts
@@ -0,0 +1,6 @@
+let Variable : string[] = ["Biryani", "Chicken Karahi", "Kunafa"];
+
+console.log(`I want to eat ${Variable[4]}`);
+// Variable 4 print undefined, becasue varible having only 3 items zero 1 and 2,
+
+console.log(`I want to eat ${Variable[2]}`);
