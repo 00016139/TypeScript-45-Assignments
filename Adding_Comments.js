a76fb69631b6c7d61a6c62415659420377232f42
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:51:49 +0500

    Add files via upload
    Question No_10

---
+++ Adding_Comments.js
@@ -0,0 +1,3 @@
+// Adeel Khan, Mar 12, 24
+var city = "Karachi"; // declare a variable
+console.log("I live in ".concat(city)); // print that variable with message
---
+++ Adding_Comments.ts
@@ -0,0 +1,3 @@
+// Adeel Khan, Mar 12, 24
+let city : string = "Karachi"; // declare a variable
+console.log(`I live in ${city}`); // print that variable with message
