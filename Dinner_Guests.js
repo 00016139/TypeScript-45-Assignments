25abbb7bfb6e9ef160e4acd698f25338ab3ddacb
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:56:21 +0500

    Add files via upload
    Question No_19

---
+++ Dinner_Guests.js
@@ -0,0 +1,19 @@
+var Guests = ["Aqib", "Fazeel", "Kashan"]; // declare a variable 
+// New Message
+console.log("WoW! I have a big table for dinner, Lets intive more friends");
+Guests.unshift("Kashan Rana");
+Guests.splice(Guests.length / 2, 0, "Saqib");
+Guests.push("Minhaj");
+Guests.forEach(function (Guest) {
+    console.log("Dear ".concat(Guest, ", you are invited for dinner"));
+}); // print the new list using array function
+// New Message
+console.log("Sorry for that, I can invite only Two friends for dinner");
+while (Guests.length > 5) {
+    var RemoveGuest = Guests.pop();
+    console.log("Sorry ".concat(RemoveGuest, ", i can't invite you for dinner today"));
+}
+Guests.forEach(function (Guest) {
+    console.log("Dear ".concat(Guest, ", you are still invited for dinner"));
+});
+console.log("I am inviting ".concat(Guests.length, " friends to dinner"));
