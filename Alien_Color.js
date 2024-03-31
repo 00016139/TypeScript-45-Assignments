0f3d9efdbda53fd40190b638e41c2e93e707d68e
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:59:58 +0500

    Add files via upload
    Question No_25

---
+++ Alien_Color.js
@@ -0,0 +1,9 @@
+var Alient_Color = "Red";
+if (Alient_Color == "Red") {
+    console.log("You Got It...!!!");
+}
+// Condition True Print the messege
+if (Alient_Color == "Blue") {
+    console.log("You Lost...!!!");
+}
+// Condition Fail
---
+++ Alien_Color.ts
@@ -0,0 +1,12 @@
+let Alient_Color :string = "Red";
+
+if(Alient_Color == "Red") {
+    console.log(`You Got It...!!!`);   
+}
+// Condition True Print the messege
+
+
+if(Alient_Color == "Blue") {
+    console.log(`You Lost...!!!`);
+}
+// Condition Fail Nothng to print
