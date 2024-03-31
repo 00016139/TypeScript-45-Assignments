53712ab35fe2dd5029bd88c95da4639a05e4cbfd
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:55:23 +0500

    Add files via upload
    Question No_17

---
+++ Shrinking_Guests_List.ts
@@ -0,0 +1,29 @@
+let Guests : string[] = ["Aqib", "Fazeel", "Kashan"]; // declare a variable 
+
+// New Message
+console.log(`WoW! I have a big table for dinner, Lets intive more friends`);
+
+Guests.unshift("Kashan Rana");
+Guests.splice(Guests.length / 2, 0, "Saqib");
+Guests.push("Minhaj");
+
+Guests.forEach(Guest =>{
+    console.log(`Dear ${Guest}, you are invited for dinner`);
+}) // print the new list using array function
+
+// New Message
+console.log("Sorry for that, I can invite only Two friends for dinner");
+
+while(Guests.length > 5) {
+    let RemoveGuest = Guests.pop();
+console.log(`Sorry ${RemoveGuest}, i can't invite you for dinner today`);
+}
+// using while loop givien condition if guests list more then 5 then remove excess friends
+// and print the message to that friend i can't invite you for dinner
+
+Guests.forEach(Guest => {
+    console.log(`Dear ${Guest}, you are still invited for dinner`);
+})
+
+Guests.splice(0 , Guests.length);
+console.log(Guests);
