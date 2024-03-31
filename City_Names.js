bdc4260cb6c23ef74ef09b8a010b210088614423
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:08:21 +0500

    Add files via upload
    Question No_39

---
+++ City_Names.js
@@ -0,0 +1,6 @@
+function City_Country(City, Country) {
+    return "".concat(City, ", ").concat(Country);
+}
+console.log(City_Country("Karachi", "Pakistan"));
+console.log(City_Country("Moscow", "Russia"));
+console.log(City_Country("Tokiyo", "Japan"));
---
+++ City_Names.ts
@@ -0,0 +1,7 @@
+function City_Country(City : string, Country : string): String {
+    return `${City}, ${Country}`;
+}
+
+console.log(City_Country("Karachi", "Pakistan"));
+console.log(City_Country("Moscow", "Russia"));
+console.log(City_Country("Tokiyo", "Japan"));
