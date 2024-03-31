9ded862b91130fc2862b5946569347fce4128794
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:42:48 +0500

    Add files via upload
    Question No_6

---
+++ Stripping_Names.ts
@@ -0,0 +1,4 @@
+let name1 : string = "\t\n Adeel \t\n" ; // declare a varible with wide space (tab \t) & new line (\n)
+console.log(name1); // line add before and after name with Tab
+console.log(name1.trim()); // print with out line and tab
+
