dc319e0121909b10b59eb632c0d54289115a1ff4
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:58:57 +0500

    Add files via upload
    Question No_24

---
+++ More_Conditional_Test.ts
@@ -0,0 +1,38 @@
+// Equility Testing with strings
+console.log("Testing Equility with Strings"); // Condition
+console.log("Adeel" == "Adeel"); // True
+
+//using Uppercase function
+console.log("Testing Uppercase Fumction");
+console.log("ADEEL".toUpperCase() == "ADEEL"); // True
+
+// Using Lowercase Function
+console.log("Testing Lowercase Function");
+console.log("adeel".toLowerCase() == "adeel"); // True
+
+// Numerical Test
+console.log("Numerical Test");
+console.log(8>5); // True
+console.log(6>=5); // True
+console.log(6<5); // False
+
+// Using "And" & "OR" Operators
+console.log("Test with 'And' and 'Or' Operators");
+console.log(true && false); // False
+
+//Test whether an item is in array
+let Friends : string[] = ["Aqib", "Fazeel", "Subhan"];
+console.log("Is Aqib your friend?"); // Condition
+console.log(Friends.includes("Aqib")); // True
+
+
+// Test whether an item is not in arra using !
+console.log("Is Kashan not your Friend"); // Condition
+console.log(!Friends.includes("Kashan")); // True
+
+
+// Checking Type of Variable
+let Name : string = "adeel";
+console.log(typeof(Name)); // String
+let Num : Number = 5
+console.log(typeof(Num)); // Number
