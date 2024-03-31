33a7c9e01ef487186e7a4b040497deb3ad74f084
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:58:28 +0500

    Add files via upload
    Question No_23

---
+++ Conditional_Tests.js
@@ -0,0 +1,17 @@
+var num1 = 5;
+var num2 = 3;
+console.log("IS num1 == 5? I Predict True"); // Condition 
+console.log(num1 == 5); // True
+console.log(num1 == 4); // False
+console.log("Is num1+num2 == 8? I Predict True"); // create condition
+console.log(num1 + num2 == 8); // True
+console.log(num1 + num2 == 6); // False
+console.log("Is num1>=5, I Predict True"); // Condition
+console.log(num1 == 5); // True
+console.log(num1 == 6); // False
+console.log("Is num1*num2 == 15, I Predict True"); // Condition
+console.log(num1 * num2 == 15); // True
+console.log(num1 * num2 == 14); // False
+console.log("Is num2>2,num2<4, I Predict True"); // Condition
+console.log(num2 == 3); // True
+console.log(num2 == 3.5); // False
