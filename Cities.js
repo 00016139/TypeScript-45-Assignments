e75f1fc94756e97bed353edfd7bc95857e52396c
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 20:07:49 +0500

    Add files via upload
    Question No_38

---
+++ Cities.js
@@ -0,0 +1,7 @@
+function Describe_Cities(City, Country) {
+    if (Country === void 0) { Country = "Pakistan"; }
+    console.log("".concat(City, " is in ").concat(Country));
+}
+Describe_Cities("Karachi");
+Describe_Cities("Islamabad");
+Describe_Cities("Moscow", "Russia");
---
+++ Cities.ts
@@ -0,0 +1,6 @@
+function Describe_Cities(City : string, Country : string = "Pakistan") {
+    console.log(`${City} is in ${Country}`);
+}
+Describe_Cities("Karachi");
+Describe_Cities("Islamabad");
+Describe_Cities("Moscow", "Russia");
