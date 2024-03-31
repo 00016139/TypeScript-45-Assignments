58f1ad0ae6bd6ab5ba6ee273491b3dd3dda4c712
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:52:49 +0500

    Add files via upload
    Question No_12

---
+++ Greetings.js
@@ -0,0 +1,5 @@
+var Names = ["Aqib", "Fazeel", "Subhan", "Kashan"]; // Declare a variable having some names
+for (var _i = 0, Names_1 = Names; _i < Names_1.length; _i++) {
+    var Name = Names_1[_i];
+    console.log("Hello ".concat(Name, ", How are you going?"));
+}
---
+++ Greetings.ts
@@ -0,0 +1,4 @@
+let Names : string[] = ["Aqib", "Fazeel", "Subhan", "Kashan"]; // Declare a variable having some names
+for ( let Name of Names) {
+    console.log(`Hello ${Name}, How are you going?`);
+} 
