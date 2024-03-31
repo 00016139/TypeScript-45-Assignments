03f7c928929b9a1cc752f2b87bb664235099a77a
00016139 <160314710+00016139@users.noreply.github.com>
2024-03-31 19:37:13 +0500

    Add files via upload

---
+++ Personal_Message.js
@@ -0,0 +1,2 @@
+var variable = "Muhammad Adeel Khan"; // declare a vatiable to store some data like name
+console.log("HEllo ".concat(variable, ", Would you like to learn some Pythone today?"));
---
+++ Personal_Message.ts
@@ -0,0 +1,3 @@
+let variable : string = "Muhammad Adeel Khan"; // declare a vatiable to store some data like name
+console.log(`HEllo ${variable}, Would you like to learn some Pythone today?`);
+// print the name with some message using back tick to compose message with variable
